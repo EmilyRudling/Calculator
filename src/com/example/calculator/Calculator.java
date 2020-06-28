341d0d4545f1b88668a6d70d44f7bacf5b777aa0
Emily Rudling <mmrudling@yahoo.com>
2020-06-28 15:34:54 +0100

    Wrote a welcome message

--- src/com/example/calculator/Calculator.java
+++ src/com/example/calculator/Calculator.java
@@ -2,6 +2,6 @@ package com.example.calculator;
 
 public class Calculator {
     public static void main(String[] args) {
-        System.out.println("hello world");
+        System.out.println("welcome to the calculator!!");
     }
 }
