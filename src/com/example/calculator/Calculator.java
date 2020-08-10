1f4401757fb4447784cf6162461d2d8220c72d2c
Emily Rudling <mmrudling@yahoo.com>
2020-08-10 16:31:30 +0100

    Created a code that generates two random numbers and then either (+_*/)'s them according to the user input'

--- src/com/example/calculator/Calculator.java
+++ src/com/example/calculator/Calculator.java
@@ -2,6 +2,40 @@ package com.example.calculator;
 
 public class Calculator {
     public static void main(String[] args) {
-        System.out.println("welcome to the calculator!!");
+        System.out.println("welcome to the calculator!");
+        int rand1 = generateRandom();
+        int rand2 = generateRandom();
+        System.out.println("Number 1" + rand1);
+        System.out.println("Number 2" + rand2);
+        System.out.println("Enter add(+), subtract(-), multiply(*) or divide(/): ");
+        Scanner scanner = new Scanner(System.in);
+        String input = scanner.next();
+        System.out.println(calculateResult(rand1, rand2, input));
+    }
+
+    public static int generateRandom() {
+        Random random = new Random();
+        int randomNumber = random.nextInt(100);
+        return randomNumber;
+    }
+
+    public static String calculateResult(int int1, int int2, String maths){
+        switch (maths){
+            case "+":
+                return Integer.toString(int1 + int2);
+            break;
+            case "-":
+                return Integer.toString(int1 - int2);
+            break;
+            case "*":
+                return Integer.toString(int1 * int2);
+            break;
+            case "/":
+                return Integer.toString(int1 / int2);
+            break;
+            default:
+                return "That is not a valid input";
+            break;
+        }
     }
 }
