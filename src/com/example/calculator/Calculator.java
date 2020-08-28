deef9e092ead5771f7921760bee74f778bd61464
Emily Rudling <mmrudling@yahoo.com>
2020-08-28 17:33:39 +0100

    Added the feature that allows users to enter their own numbers

--- src/com/example/calculator/Calculator.java
+++ src/com/example/calculator/Calculator.java
@@ -5,20 +5,21 @@ import java.util.Random; //allows me to use the random number feature
 public class Calculator {
     public static void main(String[] args) {
         System.out.println("welcome to the calculator!");
-        int rand1 = generateRandom();
-        int rand2 = generateRandom();
+        int rand1 = getNumber();
+        int rand2 = getNumber();
         System.out.println("Number 1" + rand1);
-        System.out.println("Number 2" + rand2);
         System.out.println("Enter add(+), subtract(-), multiply(*) or divide(/): ");
         Scanner scanner = new Scanner(System.in);
         String input = scanner.next();
+        System.out.println("Number 2" + rand2);
         System.out.println(calculateResult(rand1, rand2, input));
     }
 
-    public static int generateRandom() {
-        Random random = new Random();
-        int randomNumber = random.nextInt(100);
-        return randomNumber;
+    public static int getNumber() {
+        System.out.println("Enter the first integer");
+        Scanner scanner = new Scanner(System.in);
+        String number = scanner.next();
+        return Integer.parseInt(number); //converts a string to an integer
     }
 
     public static String calculateResult(int int1, int int2, String maths){
